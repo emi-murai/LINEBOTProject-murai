b0e21fae95eaf19e1c83739666a2235eee5afb8d
emi-murai <emi-murai@upload-gp.co.jp>
2018-08-09 17:42:04 +0900

    a

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php
 
-// Composer‚ÅƒCƒ“ƒXƒg[ƒ‹‚µ‚½ƒ‰ƒCƒuƒ‰ƒŠ‚ðˆêŠ‡“Ç‚Ýž‚Ý
+// Composerã§ã‚¤ãƒ³ã‚¹ãƒˆãƒ¼ãƒ«ã—ãŸãƒ©ã‚¤ãƒ–ãƒ©ãƒªã‚’ä¸€æ‹¬èª­ã¿è¾¼ã¿
 require_once __DIR__ . '/vendor/autoload.php';
 
-// ƒAƒNƒZƒXƒg[ƒNƒ“‚ðŽg‚¢CurlHTTPClient‚ðƒCƒ“ƒXƒ^ƒ“ƒX‰»
+// ã‚¢ã‚¯ã‚»ã‚¹ãƒˆãƒ¼ã‚¯ãƒ³ã‚’ä½¿ã„CurlHTTPClientã‚’ã‚¤ãƒ³ã‚¹ã‚¿ãƒ³ã‚¹åŒ–
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-// CurlHTTPClient‚ÆƒV[ƒNƒŒƒbƒg‚ðŽg‚¢LINEBot‚ðƒCƒ“ƒXƒ^ƒ“ƒX‰»
+// CurlHTTPClientã¨ã‚·ãƒ¼ã‚¯ãƒ¬ãƒƒãƒˆã‚’ä½¿ã„LINEBotã‚’ã‚¤ãƒ³ã‚¹ã‚¿ãƒ³ã‚¹åŒ–
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-// LINE Messaging API‚ªƒŠƒNƒGƒXƒg‚É•t—^‚µ‚½–¼‚ðŽæ“¾
+// LINE Messaging APIãŒãƒªã‚¯ã‚¨ã‚¹ãƒˆã«ä»˜ä¸Žã—ãŸç½²åã‚’å–å¾—
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-// –¼‚ª³“–‚©ƒ`ƒFƒbƒNB³“–‚Å‚ ‚ê‚ÎƒŠƒNƒGƒXƒg‚ðƒp[ƒX‚µ”z—ñ‚Ö
-// •s³‚Å‚ ‚ê‚Î—áŠO‚Ì“à—e‚ðo—Í
-try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+// ç½²åãŒæ­£å½“ã‹ãƒã‚§ãƒƒã‚¯ã€‚æ­£å½“ã§ã‚ã‚Œã°ãƒªã‚¯ã‚¨ã‚¹ãƒˆã‚’ãƒ‘ãƒ¼ã‚¹ã—é…åˆ—ã¸
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// é…åˆ—ã«æ ¼ç´ã•ã‚ŒãŸå„ã‚¤ãƒ™ãƒ³ãƒˆã‚’ãƒ«ãƒ¼ãƒ—ã§å‡¦ç†
+foreach ($events as $event) {
+  // ãƒ†ã‚­ã‚¹ãƒˆã‚’è¿”ä¿¡
+	$bot->replyText($event->getReplyToken(), 'TextMessage');
 }
-
 ?>
